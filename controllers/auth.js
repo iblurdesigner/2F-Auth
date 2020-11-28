054e142247c4115e8f9720d2cbd6f193b5698c87
iblurdesigner <nevomen@gmail.com>
2020-11-27 22:03:35 -0500

    Login created

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,15 +10,57 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 })
 
-exports.register = (req, res) => {
-    console.log(req.body)
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        
+        if( !email || !password ) {
+            return res.status(400).render('login', {
+                message: 'Por favor ingresa un email y contraseña validos'
+            })
+        }
 
-    const { name, email, password, passwordConfirm } = req.body
+        db.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
+            console.log(results);
+            if( !results.length || !(await bcrypt.compare(password, results[0].password) ) ) {
+                res.status(401).render('login', {
+                    message: 'El e-mail o contraseña es incorrecta'
+                })
+            } else{
+                const id = results[0].id
+                
+                const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+                    expiresIn: process.env.JWT_EXPIRES_IN
+                })
+
+                console.log("The token is" + token)
+
+                const cookieOptions = {
+                    expires: new Date(
+                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                    ),
+                    httpOnly: true
+                }
+
+                res.cookie( 'jwt', token, cookieOptions )
+                res.status(200).redirect("/")
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.register = (req, res) => {
+    console.log(req.body);
+
+    const { name, email, password, passwordConfirm } = req.body;
 
     db.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
         if(error) {
             console.log(error)
-        }
+        };
 
         if(results.length > 0) {
             return res.render('register', {
@@ -31,7 +73,7 @@ exports.register = (req, res) => {
         }
 
         let hashedPassword = await bcrypt.hash(password, 8);
-        console.log(hashedPassword)
+        console.log(hashedPassword);
 
         db.query('INSERT INTO users SET ?', {name: name, email: email, password: hashedPassword}, (error, results) => {
             if(error) {
